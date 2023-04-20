c3d155bc61e3955bcc30c0ada5967eef47c22996
mahdanidn <mahdanidn@gmail.com>
2023-04-20 08:54:30 +0700

    test lagi

---
+++ .prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  tabWidth: 2,
+  importOrder: ['^data/(.*)$', '^components/(.*)$', '^[./]'],
+  importOrderBuiltinModulesToTop: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: ['jsx', 'decorators-legacy', 'typescript'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+const buildEslintCommand = filenames =>
+  `next lint --fix --file ${filenames
+    .map(f => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  // yarn prettier --config ./.prettier.config.js -w ${filenames.join(' ')}
+  '*.{js,jsx,ts,tsx,css,md,json}': filenames =>
+    `yarn prettier --config ./.prettier.config.js -w "${filenames.join('" "')}"`
+};
