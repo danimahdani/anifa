f53567743990466b147bc2971272f38db6fb7cb9
mahdanidn <mahdanidn@gmail.com>
2023-04-20 09:24:45 +0700

    test lagi

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,22 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
+const path = require("path");
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+    .join(" --file ")}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  // '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-  // // yarn prettier --config ./.prettier.config.js -w ${filenames.join(' ')}
-  // '*.{js,jsx,ts,tsx,css,md,json}': (filenames) =>
-  //   `yarn prettier --config ./.prettier.config.js -w "${filenames.join(
-  //     '" "'
-  //   )}"`,
-  // '**/*.{js,jsx,ts,tsx}': [
-  //   'yarn tsc --noEmit',
-  //   'yarn eslint .',
-  //   'yarn prettier --write .',
-  // ],
-}
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+  // yarn prettier --config ./.prettier.config.js -w ${filenames.join(' ')}
+  "*.{js,jsx,ts,tsx,css,md,json}": (filenames) =>
+    `yarn prettier --config ./.prettier.config.js -w "${filenames.join(
+      '" "'
+    )}"`,
+  "**/*.{js,jsx,ts,tsx}": ["yarn prettier --write ."],
+};
