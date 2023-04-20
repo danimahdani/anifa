8bf5d375549f6d084853e9f85edc284495df25c3
mahdanidn <mahdanidn@gmail.com>
2023-04-20 10:19:22 +0700

    prettier settings

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,18 +1,12 @@
-module.exports = {
-  singleQuote: true,
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
-  bracketSameLine: false,
-  bracketSpacing: true,
+const config = {
+  semi: false,
   tabWidth: 2,
-  importOrder: ['^data/(.*)$', '^components/(.*)$', '^[./]'],
-  importOrderBuiltinModulesToTop: true,
-  importOrderCaseInsensitive: true,
-  importOrderParserPlugins: ['jsx', 'decorators-legacy', 'typescript'],
-  importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true,
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true
-};
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'auto'
+}
+
+module.exports = config
